9c77673d50238773a5f1962c80c26f6ac7c1ac5d
fosva <fosvandermeer@hotmail.nl>
2024-12-03 12:31:01 +0100

    lse

--- 1a_calculate_demand.py
+++ 1a_calculate_demand.py
@@ -2,8 +2,9 @@
 
 import pandas as pd
 import numpy as np
-from math import sqrt, cos, sin, log
+from math import sqrt, cos, sin, log, pi, e
 from numpy import arcsin
+import matplotlib.pyplot as plt
 
 def debug(fn):
     def wrapper(*args, **kwargs):
@@ -16,29 +17,49 @@ def debug(fn):
     return wrapper
 
 #%%
-
+radian = pi/180
 airports = pd.read_csv("airport_data.csv", sep='\t', index_col=0)
 
-@debug
+
 def d(pos0: list, pos1: list):
     R = 6371
     #from the assignment:
     return 2*R*sqrt(sin((pos0[0]-pos1[0])/2)**2 + cos(pos0[0])*cos(pos1[0])*sin((pos0[1]-pos1[1])/2)**2)
 
 n = len(airports.axes[1])
-D = np.zeros([n,n])
+#Create n x n distance matrix where n is the number of airports considered.
+dist = np.zeros([n,n])
 for i in range(n):
     for j in range(n):
         poss = airports.loc[["Latitude (deg)", "Longitude (deg)"]].astype(float)
         loc0 = poss.iloc[:,i]
         loc1 = poss.iloc[:,j]
-        D[i,j] = d(loc0.to_numpy(), loc1.to_numpy())
+        dist[i,j] = d(loc0.to_numpy()*radian, loc1.to_numpy()*radian)
 
-distances = pd.DataFrame(data = D.round(2),
+distances = pd.DataFrame(data = dist.round(2),
                          index = airports.axes[1],
                          columns = airports.axes[1])
-print(distances)
-#%%
-d = pd.DataFrame(index=airports.loc["ICAO Code"])
 
-print(d.iloc[5,5])
+#%% Least squares estimation
+pop = pd.read_csv("pop.csv", sep="\t", index_col=0)
+gdp = pd.read_csv("gdp.csv", sep="\t", index_col=0)
+demand = pd.read_csv("demand.csv", sep="\t", index_col=0)
+
+years=2
+f = 1.43 # fuel price
+#matrix a will contain the values to do the least square estimation with.
+#subtle assumption that all cities/countries are in the same order for all given data.
+ac = [[1,
+      log(pop.iloc[i,y]*pop.iloc[j,y]),
+      log(gdp.iloc[i,y]*gdp.iloc[j,y]),
+      -log(f*dist[i,j]),
+      log(demand.iloc[i,j])] for i in range(n) for j in range(i+1,n) for y in range(years)]
+ac = np.array(ac)
+a = ac[:,:-1]
+c = ac[:,-1]
+
+b = np.linalg.lstsq(a, c, rcond=None)
+res = b[0]
+res[0] = e**res[0]
+print(res)
+# %% 
