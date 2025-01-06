8c69cda769c3dbde4dd821222c51d18c7c360b3e
mcotte <mcotte@student.tudelft.nl>
2025-01-06 15:53:40 +0100

    Data import en restructuring

--- Assignment2/code/assignment2.py
+++ Assignment2/code/assignment2.py
@@ -8,18 +8,29 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 Airports_path = os.path.join(script_dir, '..', 'AirportData.xlsx')
 FleetOptions_path = os.path.join(script_dir, '..', 'FleetType.xlsx')
 Demand_path = os.path.join(script_dir, '..', 'Group35.xlsx')
+
 # Read the excel files
 Airports = pd.read_excel(Airports_path, index_col=0)
 FleetOptions = pd.read_excel(FleetOptions_path, index_col=0)
-# Simplify column names for cleaner code
-new_columns = [col.split(':')[0] for col in FleetOptions.columns]
-FleetOptions.columns = new_columns
-Demand_df = pd.read_excel(Demand_path, skiprows=1:3 index_col=0)
+Demand_df = pd.read_excel(Demand_path, skiprows=range(1,5), index_col=0)
+
+# Simplify column names for better indexing
+fleet_columns = [col.split(':')[0] for col in FleetOptions.columns]
+FleetOptions.columns = fleet_columns
+demand_columns = ["Origin", "Destination"] + [str(i) for i in range(30)]
+Demand_df.columns = demand_columns
+
+# examples of how to access data
 #print(Airports.loc['Latitude (deg)', 'London'])
 #print(FleetOptions.loc["Speed [km/h]", 'Type 1'])
-print(Demand_df.columns)
+#print(Demand_df.loc[3,str(5)])
+
+# Restructure the demand data into a 3D array
 Demand = np.zeros((len(Airports.columns), len(Airports.columns),30))
 for i in range(len(Airports.columns)):
     for j in range(len(Airports.columns)):
         for k in range(30):
-            Demand[i,j,k] = Demand_df
+            df_index = i * 20 + j + 1
+            Demand[i,j,k] = Demand_df.loc[df_index, str(k)]
+# For example the demand from Airport 0 (LHR) to Airport 1 (CDG) for the 5th time slot
+#print(Demand[0,1,5])
