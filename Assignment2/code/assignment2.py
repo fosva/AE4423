9e411c15dc71bc52fdebb0dbef1a733529cda2a6
fosva <fosvandermeer@hotmail.nl>
2025-01-07 11:54:39 +0100

    naamgeving aangepast, excel read aangepast

--- Assignment2/code/assignment2.py
+++ Assignment2/code/assignment2.py
@@ -1,19 +1,14 @@
+#%%
 import pandas as pd
 import numpy as np
 import os
 
-# import input data: Airports, fleet and demand data
-# First get the path of the datafiles
-script_dir = os.path.dirname(os.path.abspath(__file__))
-Airports_path = os.path.join(script_dir, '..', 'AirportData.xlsx')
-FleetOptions_path = os.path.join(script_dir, '..', 'FleetType.xlsx')
-Demand_path = os.path.join(script_dir, '..', 'Group35.xlsx')
-
 # Read the excel files
-Airports = pd.read_excel(Airports_path, index_col=0)
-FleetOptions = pd.read_excel(FleetOptions_path, index_col=0)
-Demand_df = pd.read_excel(Demand_path, skiprows=range(1,5), index_col=0)
+Airports = pd.read_excel("AirportData.xlsx", index_col=0)
+FleetOptions = pd.read_excel("FleetType.xlsx", index_col=0)
+Demand_df = pd.read_excel("Group35.xlsx", skiprows=range(1,5), index_col=0)
 
+#%%
 # Simplify column names for better indexing
 fleet_columns = [col.split(':')[0] for col in FleetOptions.columns]
 FleetOptions.columns = fleet_columns
@@ -26,11 +21,12 @@ Demand_df.columns = demand_columns
 #print(Demand_df.loc[3,str(5)])
 
 # Restructure the demand data into a 3D array
-Demand = np.zeros((len(Airports.columns), len(Airports.columns),30))
+demand = np.zeros((len(Airports.columns), len(Airports.columns),30))
 for i in range(len(Airports.columns)):
     for j in range(len(Airports.columns)):
         for k in range(30):
             df_index = i * 20 + j + 1
-            Demand[i,j,k] = Demand_df.loc[df_index, str(k)]
+            demand[i,j,k] = Demand_df.loc[df_index, str(k)]
 # For example the demand from Airport 0 (LHR) to Airport 1 (CDG) for the 5th time slot
 #print(Demand[0,1,5])
+#%%
--- Assignment2/code/assignment2_dynamic.py
+++ Assignment2/code/assignment2_dynamic.py
@@ -1,18 +1,34 @@
 #%%
 import numpy as np
 import pandas as pd
-from gurobipy import Model, GRB, quicksum
-import time
 
-"""# Step 1: Load data from GroupX.xlsx
-file = "Group_35.xlsx"
-flights = pd.read_excel(file, sheet_name="Flights",index_col=0)  # Flight schedule
-itineraries = pd.read_excel(file, sheet_name="Itineraries")  # Passenger itineraries
-recapture = pd.read_excel(file, sheet_name="Recapture")  # Recapture rates
-num_flights = range(len(flights))
-num_itineraries = range(len(itineraries))
-num_recaptures = range(len(recapture))
-"""
+# Read the excel files
+airports = pd.read_excel("AirportData.xlsx", index_col=0)
+fleet_options = pd.read_excel("FleetType.xlsx", index_col=0)
+demand_df = pd.read_excel("Group35.xlsx", skiprows=range(1,5), index_col=0)
+
+#%%
+# Simplify column names for better indexing
+fleet_columns = [col.split(':')[0] for col in fleet_options.columns]
+fleet_options.columns = fleet_columns
+demand_columns = ["Origin", "Destination"] + [str(i) for i in range(30)]
+demand_df.columns = demand_columns
+
+# examples of how to access data
+#print(Airports.loc['Latitude (deg)', 'London'])
+#print(FleetOptions.loc["Speed [km/h]", 'Type 1'])
+#print(Demand_df.loc[3,str(5)])
+
+# Restructure the demand data into a 3D array
+demand = np.zeros((len(airports.columns), len(airports.columns),30))
+for i in range(len(airports.columns)):
+    for j in range(len(airports.columns)):
+        for k in range(30):
+            df_index = i * 20 + j + 1
+            demand[i,j,k] = demand_df.loc[df_index, str(k)]
+# For example the demand from Airport 0 (LHR) to Airport 1 (CDG) for the 5th time slot
+#print(Demand[0,1,5])
+#%%
 aircraft_types = pd.read_excel("FleetType.xlsx", index_col=0)
 
 #types[:,0] gives characteristics of aircraft type 1
@@ -25,6 +41,9 @@ hub = 3
 def f(type, demand, location = hub):
     profit, route, cargo = max([f(type, demand, location, choice) for choice in range(20)], key = lambda x: x[0])
     return profit, route, cargo 
+
+#demand d_tij demand in timeframe t from airport i to airport j.
+
 
 
 fleet = types[-1]
